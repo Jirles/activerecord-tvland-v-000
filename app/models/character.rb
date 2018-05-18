089e2e3a43885e996b445053e35faf9f288c4574
Emily Jirles <emily.jirles@gmail.com>
2018-05-18 15:30:05 +0000

    automatically backed up by learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,8 +3,12 @@ class Character < ActiveRecord::Base
   belongs_to :actor
   belongs_to :show 
   
-  def say_the_thing_you_say
-    self.catchphrase
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}")
+
+     NoMethodError:
+       undefined method `say_that_thing_you_say' for #<Character:0x000000046e6690>
+       Did you mean?  say_the_thing_you_say
   end 
 
   
